f5c6bda5ddff822574c4bb104efd3ecb9f1d10d6
Mariugo Megiani <mariugomegiani@gmail.com>
2021-12-10 16:32:36 -0300

    UserEntity unit test

--- lib/domain/repositories/get_all_users_repository.dart
+++ lib/domain/repositories/get_all_users_repository.dart
@@ -1,4 +1,4 @@
-import 'package:unique_login/domain/entities/user_entity.dart';
+import '/domain/entities/user_entity.dart';
 
 abstract class GetAllUsersRepository {
   Future<List<UserEntity>> call();
