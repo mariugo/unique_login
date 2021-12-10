2dc9af2b240f753d6bfe85cdf148a4b94046523b
Mariugo Megiani <mariugomegiani@gmail.com>
2021-12-10 16:49:54 -0300

    Moving folders

--- lib/test/usecases/login_user/login_user_usecase_test.dart
+++
@@ -1,9 +0,0 @@
-@Timeout(Duration(seconds: 5))
-
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  group('Testing API repository methods', () {
-    test('Should be able to login', () {});
-  });
-}
