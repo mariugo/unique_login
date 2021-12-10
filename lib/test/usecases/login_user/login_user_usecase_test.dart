110c3854a0146620d115b768bf50666535afc749
Mariugo Megiani <mariugomegiani@gmail.com>
2021-12-10 16:42:40 -0300

    LoginUserCase test imp

---
+++ lib/test/usecases/login_user/login_user_usecase_test.dart
@@ -0,0 +1,9 @@
+@Timeout(Duration(seconds: 5))
+
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('Testing API repository methods', () {
+    test('Should be able to login', () {});
+  });
+}
