c1cce0fc2ea217f796e2a457096250e63e3b2d6f
Mariugo Megiani <mariugomegiani@gmail.com>
2021-12-12 19:59:23 -0300

    Fixing imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:unique_login/presentation/ui/screens/splash_screen.dart';
+import 'presentation/ui/screens/splash_screen.dart';
 import 'presentation/ui/screens/home_screen.dart';
 
-main() => runApp(SplashScreen(
-    key: UniqueKey(),
-    onInitializationComplete: () {
-      runApp(const MyApp());
-    }));
+main() => runApp(
+      SplashScreen(
+          key: UniqueKey(),
+          onInitializationComplete: () {
+            runApp(const MyApp());
+          }),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
